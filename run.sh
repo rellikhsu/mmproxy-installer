ebba00f59eaf38a0b9127fb222943cff3e0eefe0
Rellik <rellik@iMac.local>
2020-03-14 14:46:13 +0800

    - update readme and run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# 0.0.0.0:25577 為 mmproxy 的監聽端端口 , 127.0.0.1:25578 則為遊戲實際運營端口 
 sudo ./go-mmproxy -l 0.0.0.0:25577 -4 127.0.0.1:25578 -6 [::1]:25578 --allowed-subnets ./path-prefixes.txt
