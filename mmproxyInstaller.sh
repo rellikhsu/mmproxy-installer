27e6721ca27e2905722c463336347661bfed03c4
Rellik <rellik@iMac.local>
2020-03-14 14:54:10 +0800

    -update mmproxyInstaller

--- mmproxyInstaller.sh
+++ mmproxyInstaller.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-
-# Env check
-GIT=`which git`
-if [ $? -ne 0 ]; then
-	echo "Please install git"
-fi
-
-GO=`which go`
-if [ $? -ne 0 ]; then
-	echo "Please install golang"
-fi
+CURL=`which curl`
 
 # Download and chmod
 mmproxyURL="https://github.com/path-network/go-mmproxy/releases/download/1.0/go-mmproxy-linux-x86_64"
-curl -L -O ${mmproxyURL}
+${CURL} -L -O ${mmproxyURL}
 mv go-mmproxy-linux-x86_64  go-mmproxy
 chmod 755 go-mmproxy
