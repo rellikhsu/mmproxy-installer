77895e92a14f64631d8cff6ae4d817df8b522265
Rellik <rellik@iMac.local>
2020-03-14 14:55:23 +0800

    - update scripts

--- mmproxyInstaller.sh
+++ mmproxyInstaller.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 CURL=`which curl`
+if [ $? -ne 0]; then
+	echo "Please install curl first"
+	exit
+fi
 
 # Download and chmod
 mmproxyURL="https://github.com/path-network/go-mmproxy/releases/download/1.0/go-mmproxy-linux-x86_64"
